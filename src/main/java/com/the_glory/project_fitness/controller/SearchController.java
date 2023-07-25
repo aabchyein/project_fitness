a93cf56ab08edef35ddd90c488f53f7adb5008df
aabchyein <abcqaz369@naver.com>
2023-07-25 15:28:03 +0900

    혜인 search

--- src/main/java/com/the_glory/project_fitness/controller/SearchController.java
+++ src/main/java/com/the_glory/project_fitness/controller/SearchController.java
@@ -14,19 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.the_glory.project_fitness.service.CarInforsService;
-import com.the_glory.project_fitness.service.CommonService;
+import com.the_glory.project_fitness.service.SearchService;
+
+
 
 @Controller
 public class SearchController {
 
-
     @Autowired
-    CommonService fitnessService;
+    SearchService searchService;
 
     @GetMapping({"/Search"})
-    public ModelAndView main(ModelAndView modelAndView) {
-        modelAndView.addObject("name", "bwh");
+    public ModelAndView main(@RequestParam Map params, ModelAndView modelAndView) {
+        Object result = searchService.selectSearch(params);
+
+        modelAndView.addObject("params", params);
+        modelAndView.addObject("result", result);
         modelAndView.setViewName("/WEB-INF/views/Fitness/Search/Search.jsp");
         return modelAndView;
     }
